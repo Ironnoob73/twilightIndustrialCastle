07de3e6c3acd64a667b6606b0bda90353b822cef
低耐久的铁砧 <67966070+Ironnoob73@users.noreply.github.com>
2024-08-13 01:26:34 +0800

    20240813DU1

--- src/main/java/dev/hail/tfic/TICBlocks.java
+++ src/main/java/dev/hail/tfic/TICBlocks.java
@@ -1,13 +1,17 @@
 package dev.hail.tfic;
 
+import com.simibubi.create.foundation.block.connected.SimpleCTBehaviour;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import com.tterrag.registrate.util.nullness.NonNullFunction;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
 
+import static com.simibubi.create.foundation.data.CreateRegistrate.connectedTextures;
 import static com.simibubi.create.foundation.data.TagGen.pickaxeOnly;
 import static com.simibubi.create.foundation.data.TagGen.tagBlockAndItem;
 import static dev.hail.tfic.TwilightIndustrialCastle.REGISTRATE;
@@ -20,15 +24,14 @@ public class TICBlocks {
 
     public static <T extends Block> BlockEntry<T> castleBlockPattern(String Name, NonNullFunction<BlockBehaviour.Properties, T> Type){
         return REGISTRATE.block(Name, Type)
-                .initialProperties(() -> Blocks.IRON_BLOCK)
-                .transform(pickaxeOnly())
-                .properties(p -> p.mapColor(MapColor.METAL)
-                .requiresCorrectToolForDrops())
-                .tag(BlockTags.NEEDS_STONE_TOOL)
-                //.tag(Tags.Blocks.STORAGE_BLOCKS)
-                .transform(tagBlockAndItem("storage_blocks/cast_stone"))
+                .properties(p -> p.mapColor(MapColor.QUARTZ)
+                        .instrument(NoteBlockInstrument.BASEDRUM)
+                        .requiresCorrectToolForDrops()
+                        .sound(SoundType.DEEPSLATE_TILES)
+                        .strength(100.0F, 50.0F))
+                .onRegister(connectedTextures(() -> new SimpleCTBehaviour(ctEntry)))
+                .item()
                 .build()
-                //.onRegister(ConnectedTex)
                 .register();
     }
     public static final BlockEntry<Block> ARTIFICIAL_CASTLE_BRICKS = castleBlockPattern("artificial_castle_bricks",Block::new);
